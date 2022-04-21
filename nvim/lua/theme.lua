aa4c185284f781d08a57094240d1fc7e97c7eaaa
Liquidmantis <liquidmantis@gmail.com>
2022-04-21 12:23:52 -0600

    fix(theme): remove dead theme setting

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -7,7 +7,7 @@ require('lualine').setup {
 }
 
 
-vim.g.colorscheme = everforest
+-- vim.g.colorscheme = 'everforest'
 vim.o.background = 'dark'
 vim.g.everforest_background = 'hard'
 vim.cmd([[ colorscheme everforest ]])
