b685bb46dc0001402f97de9e87ea2ac8b69018f0
Liquidmantis <liquidmantis@gmail.com>
2023-01-09 11:51:38 -0700

    switch cmp binding (what does this even do???) C-Space doesn`t work on anything for some reason

--- nvim/lua/liquidmantis/plugins/cmp.lua
+++ nvim/lua/liquidmantis/plugins/cmp.lua
@@ -28,7 +28,7 @@ cmp.setup({
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-s>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
