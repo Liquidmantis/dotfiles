f070a9ad5561d6a4c8f9f46d11744fc6cfe4871f
Liquidmantis <liquidmantis@gmail.com>
2023-01-16 08:18:26 -0700

    move fundamental app control to hyper mode

--- nvim/lua/liquidmantis/plugins/lspsaga.lua
+++ nvim/lua/liquidmantis/plugins/lspsaga.lua
@@ -1,1 +1,7 @@
-require('lspsaga').init_lsp_saga()
+local lspsaga_installed, lspsaga = pcall(require, 'lspsaga')
+if not lspsaga_installed then
+  vim.notify('lspsaga not installed', 'error')
+  return
+end
+
+lspsaga.setup({})
