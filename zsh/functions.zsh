9f5a6ded9cd65524efa8c30dcfaa6c82764cf06d
Liquidmantis <liquidmantis@gmail.com>
2023-08-08 07:41:37 -0600

    clean up new git functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -47,16 +47,16 @@ function mkdir-and-cd() {
 }
 
 function git-log() {
-  git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph --decorate $2
+  git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph
 }
 
 function git-log-preview() {
-  git-log | fzf --bind 'enter:execute(echo {2})+abort' --ansi --preview \ 'git show --color=always {2}'
+  git-log | fzf --bind 'enter:execute(echo {2})+abort' --ansi --preview 'git show --color=always {2}'
 }
 
 function git-file-history() {
   fzf --bind 'enter:abort' --ansi --preview \
-    'git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph --decorate {1}'
+    'git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph {1}'
 }
 
 function git-switch-search() {
@@ -66,8 +66,7 @@ function git-switch-search() {
       return
       ;;
     *)
-      git switch "$(git branch | fzf --ansi --preview \
-        'git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph --decorate {1}' | tr -d '[:space:]')"
+      git switch "$(git branch | fzf --ansi --preview  'git-log {1}' | tr -d '[:space:]')"
       return
   esac
 }
