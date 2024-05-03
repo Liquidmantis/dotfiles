3385d5b3c7cd52d401430931d8c2b4738ff9788f
Liquidmantis <liquidmantis@gmail.com>
2024-05-03 11:41:16 -0600

    add new configs

---
+++ nvim/lua/liquidmantis/plugins/movements.lua
@@ -0,0 +1,8 @@
+return {
+    'chaoren/vim-wordmotion',            -- adds CamelCase wordmotions
+    'tpope/vim-surround',
+    'tpope/vim-repeat',
+    'ggandor/leap.nvim',
+    'ggandor/flit.nvim',
+}
+
