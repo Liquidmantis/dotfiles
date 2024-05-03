52c98a76ed9c0c8012ecb371c9080f9bf2941417
Liquidmantis <liquidmantis@gmail.com>
2024-05-03 06:01:34 -0600

    add obsidian external URL handling

--- nvim/lua/liquidmantis/plugins/obsidian.lua
+++ nvim/lua/liquidmantis/plugins/obsidian.lua
@@ -37,4 +37,11 @@ obsidian.setup({
 
     use_path_only = false,
   },
+
+  ---@param url string
+  follow_url_func = function(url)
+    -- Open the URL in the default web browser.
+    vim.fn.jobstart({"open", url})  -- Mac OS
+    -- vim.fn.jobstart({"xdg-open", url})  -- linux
+  end,
 })
