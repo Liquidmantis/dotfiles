297541218562249e5aaa2f5eb7f11c7fda006258
Liquidmantis <liquidmantis@gmail.com>
2024-05-04 16:34:49 -0600

    migrate cmp and luasnip

---
+++ nvim/lua/liquidmantis/plugins/cmp.lua
@@ -0,0 +1,87 @@
+return {
+  'hrsh7th/nvim-cmp',
+
+  dependencies = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-path',
+    'saadparwaiz1/cmp_luasnip',
+    'onsails/lspkind.nvim',
+    'L3MON4D3/LuaSnip',
+  },
+
+  config = function()
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+    local lspkind = require('lspkind')
+
+    require'luasnip/loaders/from_vscode'.lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-s>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+        ['<Tab>'] = function(fallback)
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end,
+        ['<S-Tab>'] = function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+      }),
+
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol_text',
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          symbol_map = { Copilot = "" },
+          before = function (entry, vim_item)
+            return vim_item
+          end
+      })
+    },
+
+      sources = cmp.config.sources({
+        { name = 'copilot', group_index = 2 },
+        { name = 'luasnip' },
+        { name = 'nvim_lua' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'path' },
+        { name = 'buffer' },
+      }),
+
+      experimental = {
+        ghost_text = true,
+        -- native_menu = false,
+      }
+    })
+  end
+}
