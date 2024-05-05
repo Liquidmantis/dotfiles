c3cbb0209994a7bf48477d6d94881bf67f731688
Liquidmantis <liquidmantis@gmail.com>
2024-05-05 07:52:02 -0600

    clean up

--- nvim/lua/liquidmantis/plugins/movements.lua
+++ nvim/lua/liquidmantis/plugins/movements.lua
@@ -2,7 +2,5 @@ return {
     'chaoren/vim-wordmotion',            -- adds CamelCase wordmotions
     'tpope/vim-surround',
     'tpope/vim-repeat',
-    'ggandor/leap.nvim',
-    'ggandor/flit.nvim',
 }
 
