be72abc7d898824569b33406dfc17515c0253743
Liquidmantis <liquidmantis@gmail.com>
2024-05-05 07:02:50 -0600

    migrate go

--- nvim/lua/liquidmantis/core/plugins.lua
+++ nvim/lua/liquidmantis/core/plugins.lua
@@ -38,23 +38,11 @@ return {
   -- Telescope, prereqs, and extensions
     'nvim-lua/popup.nvim',
     'nvim-lua/plenary.nvim',
-      'kkharji/sqlite.lua' }},
+    'kkharji/sqlite.lua',
     'nvim-neorg/neorg-telescope',
 --
   -- Language add-ons
     'kmonad/kmonad-vim',
-    { 'ray-x/go.nvim', dependencies = {  -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-      event = {"CmdlineEnter"},
-      ft = {"go", 'gomod'},
-      build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-    },
     'PProvost/vim-ps1',
     'JayDoubleu/vim-pwsh-formatter',
     'hashivim/vim-terraform',
