01a88ed3c6c7616bdbcd729a778a4d88755ef290
Liquidmantis <liquidmantis@gmail.com>
2024-05-15 11:41:09 -0600

    restore catppuccin transparency configuration lost in migration

--- nvim/lua/liquidmantis/plugins/catppuccin.lua
+++ nvim/lua/liquidmantis/plugins/catppuccin.lua
@@ -1,3 +1,14 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin" }
+  "catppuccin/nvim",
+  name = "catppuccin",
+  config = function()
+    require('catppuccin').setup({
+      transparent_background = true,
+      integrations = {
+        nvimtree = {
+          transparent_panel = true,
+        },
+      },
+    })
+  end
 }
