1ca25898c324bb4a4c07c1539f059d4908064aeb
Liquidmantis <liquidmantis@gmail.com>
2024-05-23 07:28:22 -0600

    change cmp to use a deliberate completion action

--- nvim/lua/liquidmantis/plugins/cmp.lua
+++ nvim/lua/liquidmantis/plugins/cmp.lua
@@ -36,7 +36,7 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-s>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
+        ['<S-CR>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
