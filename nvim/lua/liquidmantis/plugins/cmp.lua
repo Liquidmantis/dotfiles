ad5f9a604ba5cdb0c892dca8a469c7c9544cdf32
Liquidmantis <liquidmantis@gmail.com>
2024-05-28 06:37:50 -0600

    fix duplicate signature help

--- nvim/lua/liquidmantis/plugins/cmp.lua
+++ nvim/lua/liquidmantis/plugins/cmp.lua
@@ -5,7 +5,7 @@ return {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
+    -- 'hrsh7th/cmp-nvim-lsp-signature-help', -- disabled as it competes with noice
     'hrsh7th/cmp-nvim-lua',
     'hrsh7th/cmp-path',
     'saadparwaiz1/cmp_luasnip',
