92ce27512543400bac6a1efa806418f642221083
Liquidmantis <liquidmantis@gmail.com>
2024-05-31 06:58:27 -0600

    improve hcloud functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -135,8 +135,35 @@ function tmux-attach-session() {
 }
 
 function hcloud-select-stack() {
-  stack="$(hcloud hashistack list --format=json | jq -r '.[].stack' | fzf-tmux -p)"
-  if [[ $1 == "setenv" ]]; then
+  all_flag=false
+  setenv_flag=false
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --all|-a)
+        all_flag=true
+        shift
+        ;;
+      --setenv|-s)
+        setenv_flag=true
+        shift
+        ;;
+      *)
+        echo "Unknown option: $1"
+        return 1
+        ;;
+      esac
+  done
+
+  if $all_flag; then
+    stacks=$(grep 'Host ' ${HOME}/.ssh/config.d/hashistack | awk '{print $2}' | sed 's/-bastion//g' | grep -v '^\*$' | sort)
+  else
+    stacks="$(hcloud hashistack list --format=json | jq -r '.[].stack')" 
+  fi
+
+  stack="$(echo $stacks | fzf-tmux -p)"
+
+  if $setenv_flag; then
     export STACK=$stack
   else
     echo $stack
@@ -149,7 +176,10 @@ function hcloud-connect-stack() {
   elif [[ -n $STACK ]]; then
     stack=$STACK
   else
-    stack=$(hcloud-select-stack)
+    stack=$(hcloud-select-stack -a)
+  fi
+  if [[ -z $stack ]]; then
+    return 1
   fi
   echo "Connecting to $stack..."
   hcloud hashistack vpn $stack
