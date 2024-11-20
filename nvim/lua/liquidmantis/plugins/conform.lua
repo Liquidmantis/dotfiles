a0fe30babb7132a744c8a3e062cbb777bef4f798
Liquidmantis <liquidmantis@gmail.com>
2024-11-20 12:04:06 -0700

    add conform autoformatting plugin

---
+++ nvim/lua/liquidmantis/plugins/conform.lua
@@ -0,0 +1,19 @@
+-- conform autoformatting
+return {
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    require("conform").setup({
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+
+      formatters_by_ft = {
+        terraform = { "terraform_fmt" },
+      }
+
+    })
+  end
+}
