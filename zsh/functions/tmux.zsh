fad18f7776f5db3bea1f409b6a65a8c424b705ec
Liquidmantis <liquidmantis@gmail.com>
2024-12-16 07:23:30 -0700

    add tmux functions

--- zsh/functions/tmux.zsh
+++ zsh/functions/tmux.zsh
@@ -3,21 +3,26 @@ function tmux-attach-session() {
     1="main"
   fi
   if [[ -n $TMUX ]]; then
+    echo "Already in a tmux session"
     if [[ $(tmux has-session -t $1) ]]; then
-      tmux switch-client -t $1 
+      echo "Attaching to session $1"
+      tmux switch-client -t $1
     else
+      echo "Creating new session $1"
       tmux new-session -s $1 -d
       tmux switch-client -t $1
     fi
     return
   fi
   if [[ $(pgrep tmux) ]]; then
+    echo "Found tmux process"
     if [[ $(tmux list-session | grep ^$1) ]]; then
       tmux attach -t $1
     else
       tmux new-session -s $1
     fi
   else
+    echo "Starting new tmux session"
     tmux new-session -s $1
   fi
 }
