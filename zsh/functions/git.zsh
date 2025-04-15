65dd198d407248ead81e314fae74874cb02a9930
Liquidmantis <liquidmantis@gmail.com>
2025-04-15 08:09:51 -0600

    git functions

--- zsh/functions/git.zsh
+++ zsh/functions/git.zsh
@@ -1,10 +1,14 @@
+function git-ignore() {
+  echo "$1" >>.gitignore
+}
+
 function git-log() {
-  git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph |
+  git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph $1 |
     bat --decorations=never
 }
 
 function git-log-preview() {
-  git-log | fzf --bind 'enter:execute(echo {2})+abort' --ansi --preview 'git show --color=always {2}'
+  git-log $1 | fzf --bind 'enter:execute(echo {2})+abort' --ansi --preview 'git show --color=always {2}'
 }
 
 function git-file-history() {
@@ -13,51 +17,54 @@ function git-file-history() {
 }
 
 function git-stash-preview() {
-git stash list | fzf --bind 'enter:abort' --ansi --preview 'awk "{print substr({1},8,1)}" | git stash show -- -p --color=always'
+  git stash list | fzf --bind 'enter:abort' --ansi --preview 'awk "{print substr({1},8,1)}" | git stash show -- -p --color=always'
 }
-
 
 function git-switch-search() {
   case $1 in
-    "all")
-      branches="$(git branch --all)"
-      ;;
-    *)
-      branches="$(git branch)"
-      ;;
+  "all")
+    branches="$(git branch --all)"
+    ;;
+  *)
+    branches="$(git branch)"
+    ;;
   esac
-  branch="$(echo $branches | 
-        egrep -v "(dependabot)" | 
-        fzf --ansi --preview  \
-        'git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph {1}' | 
-        sed 's|^*||' | 
-        tr -d '[:space:]')"
-        # awk -F'/' '{print $NF}' | 
+  branch="$(echo $branches |
+    egrep -v "(dependabot)" |
+    fzf --ansi --preview \
+      'git log --color=always --pretty=format:"%C(yellow)%h %C(red)%ad %C(blue)%an%C(auto)%d %Creset%s" --date=short --graph {1}' |
+    sed 's|^*||' |
+    tr -d '[:space:]')"
+  # awk -F'/' '{print $NF}' |
 
   git switch $branch
 }
 
 function git-branch-merged-delete() {
   case $1 in
-    "remote")
-      git branch --remote --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -dr
-      return
-      ;;
-    *)
-      git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d
-      return
+  "remote")
+    git branch --remote --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -dr
+    return
+    ;;
+  *)
+    git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d
+    return
+    ;;
   esac
 }
 
 function git-branch-orphans() {
   case $1 in
-    "list-only")
-      git fetch -p ; git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}'
-      return
-      ;;
-    "delete")
-      git fetch -p ; git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
-      return
+  "list-only")
+    git fetch -p
+    git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}'
+    return
+    ;;
+  "delete")
+    git fetch -p
+    git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
+    return
+    ;;
   esac
 }
 
