0e2f47e96ce45428d7db8da6fbe807cf735d488e
Liquidmantis <liquidmantis@gmail.com>
2025-05-11 17:02:13 -0600

    add snacks image for latex rendering

--- nvim/lua/liquidmantis/plugins/render-markdown.lua
+++ nvim/lua/liquidmantis/plugins/render-markdown.lua
@@ -5,5 +5,9 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },   -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    latex = {
+      enabled = false,
+    }
+  },
 }
