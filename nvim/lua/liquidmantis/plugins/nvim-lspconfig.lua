10140bcbd4b744a7ecb5e4551d55890cc53b8d97
Liquidmantis <liquidmantis@gmail.com>
2025-10-06 09:46:59 -0600

    refactor lsp to use native config

--- nvim/lua/liquidmantis/plugins/nvim-lspconfig.lua
+++ nvim/lua/liquidmantis/plugins/nvim-lspconfig.lua
@@ -6,147 +6,88 @@ return {
   },
 
   config = function()
-    local lspconfig = require 'lspconfig'
+    local lsp = vim.lsp
     local util = require 'lspconfig/util'
     local configs = require 'lspconfig/configs'
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
     require('mason').setup()
 
-    lspconfig.asm_lsp.setup {}
-    lspconfig.clangd.setup {}
-    lspconfig.cssls.setup {}
-    lspconfig.docker_compose_language_service.setup {}
-    lspconfig.dockerls.setup {}
-    lspconfig.html.setup {}
+    lsp.enable('docker_compose_language_service')
+    lsp.enable('dockerls')
+    lsp.enable('html')
 
     -- Bash
-    lspconfig.bashls.setup {
+    lsp.config('bashls', {
       filetypes = { "sh", "zsh" },
-      root_dir = util.root_pattern(".bashrc", ".zshrc"),
-    }
+      root_markers = { ".bashrc", ".zshrc" }
+    })
+    lsp.enable('bashls')
 
     -- Terraform
-    lspconfig.terraformls.setup {
+    lsp.config('terraformls', {
       cmd = { "terraform-ls", "serve" },
       filetypes = { "terraform", "tf", "terraform-vars" },
-    }
+    })
+    lsp.enable('terraformls')
 
-    -- Markdown
-    require 'lspconfig'.marksman.setup {}
+    lsp.enable('marksman')
+    lsp.enable('gopls')
+    lsp.enable('gdscript')
+    lsp.enable('lua_ls')
 
-    lspconfig.omnisharp.setup {
-      on_attach = function(_, bufnr)
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-      end,
-      -- root_dir = util.root_pattern(".cs"),
-      cmd = { "/usr/local/bin/mono", "/usr/local/bin/omnisharp-manual/omnisharp/OmniSharp.exe", "--languageserver" },
-    }
+    lsp.config('pico8_ls', {
+      filetypes = { "p8", 'pico8', 'lua' },
+    })
+    lsp.enable('pico8_ls')
 
-    -- lspconfig.golangci_lint_ls.setup{}
-
-    lspconfig.gopls.setup {
-      cmd = { "gopls", "serve" },
-      filetypes = { "go", "gomod" },
-      root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-      settings = {
-        gopls = {
-          experimentalPostfixCompletions = true,
-          analyses = {
-            unusedparams = true,
-            shadow = true,
-          },
-          completeUnimported = true,
-          staticcheck = true,
-          usePlaceholders = true,
-        },
-      },
-      init_options = {
-        usePlaceholders = true,
-      },
-    }
-
-    lspconfig.gdscript.setup { capabilities = capabilities }
-
-    -- require'lspconfig'.emmet_language_server.setup{}
-    -- if not lspconfig.emmet_ls then
-    --   configs.emmet_ls = {
-    --     default_config = {
-    --       cmd = {'emmet-ls', '--stdio'};
-    --       filetypes = {'html', 'css', 'blade'};
-    --       -- root_dir = function(fname)
-    --       --   return vim.loop.cwd()
-    --       -- end;
-    --       settings = {};
-    --     };
-    --   }
-    -- end
-    -- lspconfig.emmet_ls.setup{ capabilities = capabilities; }
-
-    -- Python
-    lspconfig.pyright.setup {}
+    lsp.enable('pyright')
 
     -- TypeScript
-    lspconfig.ts_ls.setup {
-      root_dir = require 'lspconfig'.util.root_pattern("sketch.js", "index.html")
-    }
-
-    -- PowerShell
-    lspconfig.powershell_es.setup {
-      bundle_path = '/usr/local/share/powershell_es',
-      filetypes = { "powershell", "ps1" },
-    }
+    -- lspconfig.ts_ls.setup {
+    --   root_dir = require 'lspconfig'.util.root_pattern("sketch.js", "index.html")
+    -- }
 
     -- Lua
-    local runtime_path = vim.split(package.path, ';')
-    table.insert(runtime_path, "lua/?.lua")
-    table.insert(runtime_path, "lua/?/init.lua")
+    -- local runtime_path = vim.split(package.path, ';')
+    -- table.insert(runtime_path, "lua/?.lua")
+    -- table.insert(runtime_path, "lua/?/init.lua")
 
-    lspconfig.lua_ls.setup {
-      settings = {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
-          },
-          diagnostics = {
-            globals = { 'vim', 'hs' },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = {
-              -- vim.api.nvim_get_runtime_file("", true),
-              vim.env.VIMRUNTIME,
-              "${3rd}/luv/library",
-              vim.fn.expand("$VIMRUNTIME/lua"),
-              "~/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
-            },
-            checkThirdParty = false,
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    }
+    -- lspconfig.lua_ls.setup {
+    --   settings = {
+    --     Lua = {
+    --       runtime = {
+    --         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+    --         version = 'LuaJIT',
+    --         -- Setup your lua path
+    --         path = runtime_path,
+    --       },
+    --       diagnostics = {
+    --         globals = { 'vim', 'hs' },
+    --       },
+    --       workspace = {
+    --         -- Make the server aware of Neovim runtime files
+    --         library = {
+    --           -- vim.api.nvim_get_runtime_file("", true),
+    --           vim.env.VIMRUNTIME,
+    --           "${3rd}/luv/library",
+    --           vim.fn.expand("$VIMRUNTIME/lua"),
+    --           "~/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
+    --         },
+    --         checkThirdParty = false,
+    --       },
+    --       -- Do not send telemetry data containing a randomized but unique identifier
+    --       telemetry = {
+    --         enable = false,
+    --       },
+    --     },
+    --   },
+    -- }
 
-    lspconfig.rust_analyzer.setup({
-      settings = {
-        ['rust-analyzer'] = {
-          cargo = { allFeatures = true },
-          checkOnSave = {
-            command = "clippy",
-          },
-        },
-      },
-    })
-
-    lspconfig.yamlls.setup {
+    lsp.config('yamlls', {
       filetypes = { "yaml", "yml" },
-    }
+    })
+    lsp.enable('yamlls')
   end
 }
