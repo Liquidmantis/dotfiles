f1471f65f0426595b3b8729134e0c8971c3ac3f5
Liquidmantis <liquidmantis@gmail.com>
2025-10-08 10:55:11 -0600

    lsp cleanup and migration to vim.lsp

--- nvim/lua/liquidmantis/plugins/nvim-lspconfig.lua
+++ nvim/lua/liquidmantis/plugins/nvim-lspconfig.lua
@@ -7,8 +7,6 @@ return {
 
   config = function()
     local lsp = vim.lsp
-    local util = require 'lspconfig/util'
-    local configs = require 'lspconfig/configs'
     -- local capabilities = vim.lsp.protocol.make_client_capabilities()
     -- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
@@ -35,7 +33,6 @@ return {
     lsp.enable('marksman')
     lsp.enable('gopls')
     lsp.enable('gdscript')
-    lsp.enable('lua_ls')
 
     lsp.config('pico8_ls', {
       filetypes = { "p8", 'pico8', 'lua' },
@@ -49,41 +46,45 @@ return {
     --   root_dir = require 'lspconfig'.util.root_pattern("sketch.js", "index.html")
     -- }
 
-    -- Lua
-    -- local runtime_path = vim.split(package.path, ';')
-    -- table.insert(runtime_path, "lua/?.lua")
-    -- table.insert(runtime_path, "lua/?/init.lua")
+    lsp.config('lua_ls', {
+      on_init = function(client)
+        if client.workspace_folders then
+          local path = client.workspace_folders[1].name
+          if
+              path ~= vim.fn.stdpath('config')
+              and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+          then
+            return
+          end
+        end
 
-    -- lspconfig.lua_ls.setup {
-    --   settings = {
-    --     Lua = {
-    --       runtime = {
-    --         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-    --         version = 'LuaJIT',
-    --         -- Setup your lua path
-    --         path = runtime_path,
-    --       },
-    --       diagnostics = {
-    --         globals = { 'vim', 'hs' },
-    --       },
-    --       workspace = {
-    --         -- Make the server aware of Neovim runtime files
-    --         library = {
-    --           -- vim.api.nvim_get_runtime_file("", true),
-    --           vim.env.VIMRUNTIME,
-    --           "${3rd}/luv/library",
-    --           vim.fn.expand("$VIMRUNTIME/lua"),
-    --           "~/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
-    --         },
-    --         checkThirdParty = false,
-    --       },
-    --       -- Do not send telemetry data containing a randomized but unique identifier
-    --       telemetry = {
-    --         enable = false,
-    --       },
-    --     },
-    --   },
-    -- }
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            version = 'LuaJIT',
+            -- Tell the language server how to find Lua modules same way as Neovim
+            -- (see `:h lua-module-load`)
+            path = {
+              'lua/?.lua',
+              'lua/?/init.lua',
+            },
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+              "${3rd}/luv/library",
+              --           vim.fn.expand("$VIMRUNTIME/lua"),
+              "~/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
+            }
+          }
+        })
+      end,
+      settings = {
+        Lua = {}
+      }
+    })
+    lsp.enable('lua_ls')
 
     lsp.config('yamlls', {
       filetypes = { "yaml", "yml" },
