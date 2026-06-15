24ec2548d296f867ac111ce34e1f73b64564192f
Liquidmantis <liquidmantis@gmail.com>
2026-06-15 07:38:17 -0600

    disable image.nvim in neovide

--- nvim/lua/liquidmantis/plugins/image.lua
+++ nvim/lua/liquidmantis/plugins/image.lua
@@ -1,9 +1,13 @@
-return {
-  "3rd/image.nvim",
-  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-  opts = {
-    processor = "magick_cli",
-    max_height_window_percentage = math.huge,
-    max_width_window_percentage = math.huge
-  },
-}
+if not vim.g.neovide then
+  return {
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = "magick_cli",
+      max_height_window_percentage = math.huge,
+      max_width_window_percentage = math.huge
+    },
+  }
+else
+  return {}
+end
