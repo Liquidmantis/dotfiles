dd58169eb62fb1a33b1e38113a264f18da6df084
Liquidmantis <liquidmantis@gmail.com>
2026-09-22 06:00:12 -0600

    quick catchup

--- hammerspoon/bindings/hyperapp.lua
+++ hammerspoon/bindings/hyperapp.lua
@@ -6,7 +6,7 @@
 -- ****************************************
 
 Modes.HyperApp:bind('', 'a', function()
-  Utils.launch_hide_or_focus('boltai 2')
+  Utils.launch_hide_or_focus('ChatGPT')
   -- local raycast_link =
   -- 'raycast://extensions/raycast/raycast-ai/ai-chat?context=%7B%22preset%22:%2284E28514-4A3B-49F4-AF8C-492E5CF05FCF%22%7D'
   -- local command = "open '" .. raycast_link .. "'"
